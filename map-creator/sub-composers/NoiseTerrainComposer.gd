706ed48c6795e4eea0136f2d5028180a30fffd5a
dave <cospan@gmail.com>
2024-08-21 13:01:05 -0400

    WIP Figuring out collisions

--- map-creator/sub-composers/NoiseTerrainComposer.gd
+++ map-creator/sub-composers/NoiseTerrainComposer.gd
@@ -54,7 +54,107 @@ func collision(local_mesh:MeshInstance3D, other_mesh:MeshInstance3D):
     # Need to figure out where this collision occurred
     var local_aabb = local_mesh.get_aabb()
     var other_aabb = other_mesh.get_aabb()
+    var local_rect = Rect2(local_aabb.position.x, local_aabb.position.z, local_aabb.size.x, local_aabb.size.z)
+    var other_rect = Rect2(other_aabb.position.x, other_aabb.position.z, other_aabb.size.x, other_aabb.size.z)
     m_logger.debug("  Position: %s" % local_aabb.position)
+    #var intersection = local_aabb.intersection(other_aabb)
+    var intersection = local_rect.intersection(other_rect)
+    m_logger.debug("  Intersection: %s" % intersection)
+    m_logger.debug("  Encloses: %s" % local_rect.encloses(other_rect))
+    # Iterate through the corners of the other rect to see if they are inside the local rect
+    var other_corners = [
+        Vector2(other_rect.position.x, other_rect.position.y),
+        Vector2(other_rect.position.x + other_rect.size.x, other_rect.position.y),
+        Vector2(other_rect.position.x, other_rect.position.y + other_rect.size.y),
+        Vector2(other_rect.position.x + other_rect.size.x, other_rect.position.y + other_rect.size.y)
+    ]
+    var other_corner_hits = [false, false, false, false]
+    var hit_count = 0
+    for i in range(0, 4):
+        other_corner_hits[i] = local_rect.has_point(other_corners[i])
+        hit_count += 1 if other_corner_hits[i] else 0
+
+    var new_rects = []
+    # We know which corners are inside the rect, now we need to figure out which side of the rect they are on
+    #m_logger.debug("  Corner Hits: %s" % str(other_corner_hits))
+    if hit_count == 1:
+        m_logger.debug("  A Corner Inside: 2 New Blocks")
+        # There are four ways we can have only 1 corner inside the rect
+        # 1. Top Left:     other_corners[3]
+        # 2. Top Right:    other_corners[2]
+        # 3. Bottom Left:  other_corners[1]
+        # 4. Bottom Right: other_corners[0]
+        var A:Rect2
+        var B:Rect2
+
+        # If it's top left, then we need to create two blocks
+        # A: or.e.x, lr.y,   lr.e.x, lr.e.y
+        # B: lr.x,   or.e.y, or.e.x, lr.e.y
+        A = Rect2(other_rect.end.x,
+                  local_rect.position.y,
+                  local_rect.end.x - other_rect.end.x,
+                  local_rect.size.y)
+        B = Rect2(local_rect.position.x,
+                  other_rect.end.y,
+                  other_rect.end.x - local_rect.position.x,
+                  local_rect.size.y - other_rect.end.y)
+
+        # If it's top right, then we need to create two blocks
+        # A: lr.x, lr.y,   or.x,   lr.e.y
+        # B: or.x, or.e.y, lr.e.x, lr.e.y
+
+        # If it's bottom left, then we need to create two blocks
+        # A: lr.x, lr.y,   lr.e.x, or.y
+        # B: or.e.x, or.y, lr.e.x, lr.e.y
+
+        # If it's bottom right, then we need to create two blocks
+        # A: lr.x, lr.y,   lr.e.x, or.y
+        # B: lr.x, or.y,   or.x,   lr.e.y
+
+
+    elif hit_count == 2:
+        m_logger.debug("  Two Corners Inside: 3 New Blocks")
+    elif hit_count == 3:
+        m_logger.debug("  Three Corners Inside: 3 New Blocks (Not Possible)") 
+    elif hit_count == 4:
+        m_logger.debug("  All Corners Inside: 4 New Blocks")
+        #Create 4 rectangles A, B, C, D (Corners)
+        # A: lr.x,   lr.y,   lr.e.x, or.y
+        # B: lr.x,   or.y,   or.x,   lr.e.y
+        # C: or.x,   or.e.y, lr.e.x, lr.e.y
+        # D: or.e.x, or.y,   lr.e.x, or.e.y
+
+        # These don't look right
+        var A = Rect2(local_rect.position.x,
+                      local_rect.position.y,
+                      local_rect.size.x,
+                      other_rect.position.y - local_rect.position.y)
+
+        var B = Rect2(local_rect.position.x,
+                      other_rect.position.y,
+                      other_rect.position.x - local_rect.position.x,
+                      local_rect.end.y - other_rect.position.y)
+
+        var C = Rect2(other_rect.position.x,
+                      other_rect.end.y,
+                      local_rect.end.x - other_rect.position.x,
+                      local_rect.end.y - other_rect.end.y)
+
+        var D = Rect2(other_rect.end.x,
+                      other_rect.position.y,
+                      local_rect.end.x - other_rect.end.x,
+                      other_rect.end.y - other_rect.position.y)
+        new_rects.append(A)
+        new_rects.append(B)
+        new_rects.append(C)
+        new_rects.append(D)
+        m_logger.debug("  New Rects: %s" % str(new_rects))
+
+    elif hit_count == 0:
+        m_logger.debug("  No Corners Inside")
+
+
+
 
 
 
